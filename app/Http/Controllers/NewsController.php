597b8a98b7f94baa13b99733918e14d076e3643b
Nadheem <nadheem@mavens-i.com>
2023-08-11 18:24:59 +0530

    Laravel API New Project

---
+++ app/Http/Controllers/NewsController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\News;
+
+class NewsController extends Controller
+{
+    public function index(){
+        $news = News::all();
+
+        return response()->json([
+            'data' => $news
+        ]);
+    }
+}
---
+++ database/migrations/2023_08_09_125441_create_news_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('news', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug');
+            $table->string('image');
+            $table->text('description');
+            $table->string('createdby')->nullable(true);
+            $table->string('updatedby')->nullable(true);
+            $table->dateTime('createdon', $precision = 0)->useCurrent();
+            $table->dateTime('updatedon', $precision = 0)->nullable(true)->useCurrentOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('news');
+    }
+};
